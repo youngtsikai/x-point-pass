9917ef5714f83407ddc66b4feabab690ab4f5393
Young Tsikai <keithtsikai0@gmail.com>
2025-05-09 14:55:00 +0200

    9 May 14:54

---
+++ src/app/staff-sign-in/staff-sign-in.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { MainHeaderComponent } from '../main-header/main-header.component';
+import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-staff-sign-in',
+  imports: [MainHeaderComponent, RouterModule, CommonModule, FormsModule],
+  templateUrl: './staff-sign-in.component.html',
+  styleUrl: './staff-sign-in.component.css',
+})
+export class StaffSignInComponent {
+  private authserv = inject(AuthService);
+  private rout = inject(Router);
+
+  userData = {
+    email: '',
+    password: '',
+  };
+
+  onSubmit(): void {
+    this.authserv
+      .signin(this.userData.email, this.userData.password)
+      .subscribe({
+        next: () => {
+          this.rout.navigateByUrl('/staff-dashboard');
+        },
+        error: (error) => {
+          console.error('Sign-in failed:', error);
+          let errorMessage = 'Sign-in Failed. Please try again.';
+          if (error & error.message) {
+            errorMessage = error.message;
+          }
+          alert(errorMessage);
+        },
+      });
+  }
+}
